e309703914a64ee0bc6101dc3429c2a075bd776e
mikayil aktemur <mikayil34@gmail.com>
2022-11-19 21:37:08 +0300

    edit

---
+++ food-ordering/components/layout/Header.jsx
@@ -0,0 +1,32 @@
+import Logo from "../ui/Logo";
+ 
+const Header = () => {
+  return (
+    <div className="h-[5.5rem] bg-secondary">
+      <div className="container mx-auto text-white flex justify-between items-center h-full">
+        <Logo />
+        <nav>
+          <ul className="flex gap-x-2">
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Home</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Menu</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">About</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Book Table</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-x-4 items-center">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ food-ordering/components/ui/logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const logo = () => {
+  return <div className="text-[2rem] font-dancing font-bold">Feane</div>;
+};
+
+export default logo;
