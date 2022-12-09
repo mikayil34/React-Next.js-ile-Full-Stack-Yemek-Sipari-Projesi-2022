028cc3e56e5eb1d4e5e9be629c677633d0873237
Mikayil Aktemur <mikayil34@gmail.com>
2022-12-09 22:25:54 +0300

    edit

--- food-ordering/components/ui/logo.jsx
+++ food-ordering/components/ui/logo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
-const logo = () => {
-  return <div className="text-[2rem] font-dancing font-bold">Feane</div>;
+const Logo = () => {
+  return (
+    <Link href="/">
+      <span className="text-[2rem] font-dancing font-bold cursor-pointer">
+        Feane
+      </span>
+    </Link>
+  );
 };
 
-export default logo;
+export default Logo;
